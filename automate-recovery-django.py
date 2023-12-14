2e65e315c94b2cdbdb1d53ef49df4e70792ba2b6
Rodrigo de Oliveira <rodrigo@rodrigo>
2023-12-14 13:17:30 -0300

    Autorização nginx

--- automate-recovery-django.py
+++ automate-recovery-django.py
@@ -4,12 +4,14 @@ import json
 
 URL_BASE = ""
 AUTH_KEY = ""
+NGINX_USER_NAME = ""
+NGINX_PASSWORD = ""
 
 headers = {'Content-Type' : 'application/json', 'Authorization' : f'Token {AUTH_KEY}'}
 
 def find_incident_id(name):
 	url_incident = URL_BASE + "/api/incidents/incidents/"
-	response = requests.get(url_incident, headers=headers)
+	response = requests.get(url_incident, headers=headers, auth=(NGINX_USER_NAME, NGINX_PASSWORD))
 
 	if 200 <= response.status_code <= 299:
 		data = response.json()
@@ -29,7 +31,7 @@ def update_incident_status(incident_id, product_id):
 		incident_data = {
 			'status' : 'resolved',
 		}
-		incident_response = requests.patch(url_incident, headers=headers, data=json.dumps(incident_data))
+		incident_response = requests.patch(url_incident, headers=headers, data=json.dumps(incident_data), auth=(NGINX_USER_NAME, NGINX_PASSWORD))
 
 		url_update = URL_BASE + "/api/incidents/incident-updates/"
 		update_data = {
@@ -39,13 +41,13 @@ def update_incident_status(incident_id, product_id):
 	        'text': 'Resolvido',
 	        'status': 'resolved',
 		}
-		update_response = requests.post(url_update, headers=headers, data=json.dumps(update_data))
+		update_response = requests.post(url_update, headers=headers, data=json.dumps(update_data), auth=(NGINX_USER_NAME, NGINX_PASSWORD))
 
 		url_component = URL_BASE + "/api/components/components/" + str(product_id) + "/"
 		component_data = {
 			'status' : 'operational'
 		}
-		component_response = requests.patch(url_component, headers=headers, data=json.dumps(component_data))
+		component_response = requests.patch(url_component, headers=headers, data=json.dumps(component_data), auth=(NGINX_USER_NAME, NGINX_PASSWORD))
 
 name_tag = sys.argv[1] if len(sys.argv) > 1 else None
 product_id_tag = sys.argv[2] if len(sys.argv) > 2 else None
